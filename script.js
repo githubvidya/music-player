ca012f5f24c20985f5da52b35ba132df26aaf9b9
Vidya Bhushan Tiwari <110774801+githubvidya@users.noreply.github.com>
2024-02-12 16:56:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,152 @@
+var arr = [
+    {
+      image: "https://images.unsplash.com/photo-1516571168827-259105cb8327?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fHN1bnJpc2V8ZW58MHx8MHx8fDA%3D",
+      url: "./Camila.mp3",
+      name: "camila",
+      time: "3.55",
+    },
+    {
+      image: "https://images.unsplash.com/photo-1484712401471-05c7215830eb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGhhcHB5JTIwZ3JvdXB8ZW58MHx8MHx8fDA%3D",
+      url: "./I Like Him(MP3_160K).mp3",
+      name: "i like him",
+      time: "2.55",
+    },
+    {
+      image: "https://plus.unsplash.com/premium_photo-1664461663120-b39152ba92ae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c3VucmlzZXxlbnwwfHwwfHx8MA%3D%3D",
+      url: "./JONY - Love your voice(MP3_160K)_1.mp3",
+      name: "jony",
+      time: "3.55",
+    },
+    {
+      image: "https://images.unsplash.com/photo-1570585703502-1375b93e7a97?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGhhcHB5JTIwZ3JvdXB8ZW58MHx8MHx8fDA%3D",
+      url: "./jhanjhariya.mp3",
+      name: "ham diwana",
+      time: "4.5",
+    },
+    {
+        image: "https://images.unsplash.com/photo-1484712401471-05c7215830eb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGhhcHB5JTIwZ3JvdXB8ZW58MHx8MHx8fDA%3D",
+        url: "./I Like Him(MP3_160K).mp3",
+        name: "i like him",
+        time: "2.55",
+      },
+      {
+        image: "https://plus.unsplash.com/premium_photo-1664461663120-b39152ba92ae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c3VucmlzZXxlbnwwfHwwfHx8MA%3D%3D",
+        url: "./JONY - Love your voice(MP3_160K)_1.mp3",
+        name: "jony",
+        time: "3.55",
+      },
+      {
+        image: "https://images.unsplash.com/photo-1570585703502-1375b93e7a97?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGhhcHB5JTIwZ3JvdXB8ZW58MHx8MHx8fDA%3D",
+        url: "./jhanjhariya.mp3",
+        name: "ham diwana",
+        time: "4.5",
+      },
+      {
+        image: "https://images.unsplash.com/photo-1484712401471-05c7215830eb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGhhcHB5JTIwZ3JvdXB8ZW58MHx8MHx8fDA%3D",
+        url: "./I Like Him(MP3_160K).mp3",
+        name: "i like him",
+        time: "2.55",
+      },
+      {
+        image: "https://plus.unsplash.com/premium_photo-1664461663120-b39152ba92ae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c3VucmlzZXxlbnwwfHwwfHx8MA%3D%3D",
+        url: "./JONY - Love your voice(MP3_160K)_1.mp3",
+        name: "jony",
+        time: "3.55",
+      },
+      {
+        image: "https://images.unsplash.com/photo-1570585703502-1375b93e7a97?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGhhcHB5JTIwZ3JvdXB8ZW58MHx8MHx8fDA%3D",
+        url: "./jhanjhariya.mp3",
+        name: "ham diwana",
+        time: "4.5",
+      },
+      {
+        image: "https://images.unsplash.com/photo-1484712401471-05c7215830eb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGhhcHB5JTIwZ3JvdXB8ZW58MHx8MHx8fDA%3D",
+        url: "./I Like Him(MP3_160K).mp3",
+        name: "i like him",
+        time: "2.55",
+      },
+      {
+        image: "https://plus.unsplash.com/premium_photo-1664461663120-b39152ba92ae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c3VucmlzZXxlbnwwfHwwfHx8MA%3D%3D",
+        url: "./JONY - Love your voice(MP3_160K)_1.mp3",
+        name: "jony",
+        time: "3.55",
+      },
+      {
+        image: "https://images.unsplash.com/photo-1570585703502-1375b93e7a97?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGhhcHB5JTIwZ3JvdXB8ZW58MHx8MHx8fDA%3D",
+        url: "./jhanjhariya.mp3",
+        name: "ham diwana",
+        time: "4.5",
+      },
+  ];
+
+
+
+  var bg = document.querySelector(".imgg");
+  var allaudio = document.querySelector(".list");
+  var audio = new Audio();
+  var selectedSongIndex = 0;
+  var back = document.querySelector("#backward");
+  var play = document.querySelector("#play");
+  var forward = document.querySelector("#forward");
+
+
+  function mainfun() {
+    var card = "";
+    arr.forEach(function (val, index) {
+      card += `<div id=${index} class="list-right">
+                  <img src="${val.image}" alt="">
+                  <h5 class="font">${val.name}</h5>
+                  <p class="font">${val.time}</p>
+              </div>`;
+    });
+    allaudio.innerHTML = card;
+    audio.src = arr[selectedSongIndex].url;
+    bg.style.backgroundImage = `url(${arr[selectedSongIndex].image})`;
+  }
+  
+  mainfun();
+  
+  allaudio.addEventListener("click", function (event) {
+    if (event.target && event.target.nodeName == "DIV") {
+      selectedSongIndex = parseInt(event.target.id);
+      play.innerHTML =`<i class="ri-pause-fill"></i>`
+      flag = 1;
+      mainfun();
+      audio.play();
+    }
+  });
+  
+
+var flag = 0;
+  play.addEventListener("click",function(){
+    if(flag == 0){
+       play.innerHTML =`<i class="ri-pause-fill"></i>`
+       mainfun();
+       audio.play();
+       flag = 1;
+    }else{
+        play.innerHTML =`<i class="ri-play-fill"></i>`
+        mainfun();
+        audio.pause();
+        flag = 0;
+    }
+  })
+
+  forward.addEventListener("click",function(){
+    if(selectedSongIndex <= arr.length -1){
+        selectedSongIndex++
+        mainfun();
+        audio.play();
+    }else{
+        forward.style.opacity = 0.5;
+    }
+  })
+  backward.addEventListener("click",function(){
+    if(selectedSongIndex > 0){
+        selectedSongIndex--
+        mainfun();
+        audio.play();
+    }else{
+        backward.style.opacity = 0.5; 
+    }
+  })
